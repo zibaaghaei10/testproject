53f182acdfbfdba6f5023ad545669633dea6578f
amir <amir@ubuntu>
2022-03-29 15:24:17 +0430

    Add tag p text: this is my Account

--- shop.php
+++ shop.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>shop</title>
@@ -9,5 +8,6 @@
 <body>
     <h1 style="text-align:center">Welcome to our Shop Page</h1>
     <p style=color:green>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque maiores laudantium, praesentium rem enim eveniet eos dolores omnis quisquam! Laudantium dolore doloremque autem repellat magnam maxime, aut porro debitis enim?</p>
+    <p>this is my Account</p>
 </body>
 </html>
